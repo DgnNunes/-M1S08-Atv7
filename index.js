8ab0bd320e0471cc7b8aca45547e31deccfeaf43
Douglas Gilson Nunes <douglas_g_nunes@estudante.sesisenai.org.br>
2024-03-11 21:47:27 -0300

    #

---
+++ index.js
@@ -0,0 +1,8 @@
+// Obtém uma referência para o elemento pelo ID
+let meuElemento = document.getElementById("meuElemento");
+console.log(meuElemento);
+
+// Remove a classe específica
+meuElemento.classList.remove("minhaClasse");
+
+meuElemento.classList.remove("outraClasse");
